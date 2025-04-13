db642625e2b2a60c7f62872292178265388803be
Leonardo Gomes <lgdevlop@gmail.com>
2025-04-13 17:30:59 -0300

    chore(dev): 🔧 setup commitlint and husky hooks

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test',
+        'build', 'ci', 'chore', 'revert'
+      ]
+    ],
+    'scope-empty': [0, 'never'],
+    'subject-case': [0],
+  }
+};
